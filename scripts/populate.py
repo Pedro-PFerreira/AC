2bfa7fdf06bf39b253c9954922147a42abfa826b
Pedro Ferreira <up202004986@edu.fe.up.pt>
2023-09-21 10:58:31 +0100

    Finished the convert to CSV->SQL and populated the database file

--- scripts/populate.py
+++ scripts/populate.py
@@ -2,10 +2,6 @@ import pandas as pd
 
 # Read the CSV file
 awards_players = pd.read_csv('../data/awards_players.csv')
-players_teams = pd.read_csv('../data/players_teams.csv')
-players = pd.read_csv('../data/players.csv')
-series_post = pd.read_csv('../data/series_post.csv')
-teams = pd.read_csv('../data/teams.csv')
 
 # Convert the CSV files to SQL
 path = "../db/database.sql"
@@ -108,17 +104,141 @@ def populate_teams(file_path):
 
     f.close()
 
+def populate_series_post(file_path):
+
+    columns = ["year", "round", "series",
+               "tmIDWinner", "lgIDWinner",
+               "tmIDLoser", "lgIDLoser",
+                "W", "L"]
+    
+    series_post = pd.read_csv('../data/series_post.csv', usecols=columns)
+
+    f = open(file_path, "a")
+
+    for i in range(len(series_post.get('year'))):
+
+        f.write("INSERT INTO series_post(year, round, series, tmIDWinner, lgIDWinner, tmIDLoser, lgIDLoser, W, L) VALUES('{year}','{round}','{series}','{tmIDWinner}','{lgIDWinner}','{tmIDLoser}','{lgIDLoser}','{W}','{L}');\n".format(
+
+            year=series_post.get('year')[i],
+            round=series_post.get('round')[i],
+            series=series_post.get('series')[i],
+            tmIDWinner=series_post.get('tmIDWinner')[i],
+            lgIDWinner=series_post.get('lgIDWinner')[i],
+            tmIDLoser=series_post.get('tmIDLoser')[i],
+            lgIDLoser=series_post.get('lgIDLoser')[i],
+            W=series_post.get('W')[i],
+            L=series_post.get('L')[i]
+        ))
+
+    f.close()
 
 
-#populate_teams_post(test_path)
-#populate_coaches(test_path)
+def populate_players_teams(file_path):
+
+    columns = ["playerID", "year", "stint",
+              "tmID", "lgID", "GP", "GS",
+              "minutes", "points", "oRebounds",
+              "dRebounds", "rebounds", "assists",
+              "steals", "blocks", "turnovers",
+              "PF", "fgAttempted", "fgMade",
+              "ftAttempted", "ftMade", "threeAttempted",
+              "threeMade", "dq","PostGP",
+              "PostGS", "PostMinutes",
+              "PostPoints", "PostoRebounds",
+              "PostdRebounds"
+              ]
+    
+    players_teams = pd.read_csv('../data/players_teams.csv', usecols=columns)
+
+    f = open(file_path, "a")
+
+    for i in range(len(players_teams.get('playerID'))):
+
+        f.write("INSERT INTO players_teams(playerID, year, stint, tmID, lgID, GP, GS, minutes, points, oRebounds, dRebounds, rebounds, assists, steals, blocks, turnovers, PF, fgAttempted, fgMade, ftAttempted, ftMade, threeAttempted, threeMade, dq, PostGP, PostGS, PostMinutes, PostPoints, PostoRebounds, PostdRebounds) VALUES('{playerID}','{year}','{stint}','{tmID}','{lgID}','{GP}','{GS}','{minutes}','{points}','{oRebounds}','{dRebounds}','{rebounds}','{assists}','{steals}','{blocks}','{turnovers}','{PF}','{fgAttempted}','{fgMade}','{ftAttempted}','{ftMade}','{threeAttempted}','{threeMade}','{dq}','{PostGP}','{PostGS}','{PostMinutes}','{PostPoints}','{PostoRebounds}','{PostdRebounds}');\n".format(
+            playerID=players_teams.get('playerID')[i],
+            year=players_teams.get('year')[i],
+            stint=players_teams.get('stint')[i],
+            tmID=players_teams.get('tmID')[i],
+            lgID=players_teams.get('lgID')[i],
+            GP=players_teams.get('GP')[i],
+            GS=players_teams.get('GS')[i],
+            minutes=players_teams.get('minutes')[i],
+            points=players_teams.get('points')[i],
+            oRebounds=players_teams.get('oRebounds')[i],
+            dRebounds=players_teams.get('dRebounds')[i],
+            rebounds=players_teams.get('rebounds')[i],
+            assists=players_teams.get('assists')[i],
+            steals=players_teams.get('steals')[i],
+            blocks=players_teams.get('blocks')[i],
+            turnovers=players_teams.get('turnovers')[i],
+            PF=players_teams.get('PF')[i],
+            fgAttempted=players_teams.get('fgAttempted')[i],
+            fgMade=players_teams.get('fgMade')[i],
+            ftAttempted=players_teams.get('ftAttempted')[i],
+            ftMade=players_teams.get('ftMade')[i],
+            threeAttempted=players_teams.get('threeAttempted')[i],
+            threeMade=players_teams.get('threeMade')[i],
+            dq=players_teams.get('dq')[i],
+            PostGP=players_teams.get('PostGP')[i],
+            PostGS=players_teams.get('PostGS')[i],
+            PostMinutes=players_teams.get('PostMinutes')[i],
+            PostPoints=players_teams.get('PostPoints')[i],
+            PostoRebounds=players_teams.get('PostoRebounds')[i],
+            PostdRebounds=players_teams.get('PostdRebounds')[i]
+        ))
+
+    f.close()
+    
+def populate_players(file_path):
+
+    columns = ["bioID", "pos", "firstseason", "lastseason",
+              "height", "weight", "birthDate", "college",
+              "collegeOther", "birthDate", "deathDate"  
+              ]
+
+    players = pd.read_csv('../data/players.csv', usecols=columns)
+
+    f = open(file_path, "a")
+
+    for i in range(len(players.get('bioID'))):
+        f.write("INSERT INTO players(bioID, pos, firstseason, lastseason, height, weight, college, collegeOther, birthDate, deathDate) VALUES('{bioID}','{pos}','{firstseason}','{lastseason}','{height}','{weight}','{college}','{collegeOther}','{birthDate}','{deathDate}');\n".format(
+            bioID=players.get('bioID')[i],
+            pos=players.get('pos')[i],
+            firstseason=players.get('firstseason')[i],
+            lastseason=players.get('lastseason')[i],
+            height=players.get('height')[i],
+            weight=players.get('weight')[i],
+            college=players.get('college')[i],
+            collegeOther=players.get('collegeOther')[i],
+            birthDate=players.get('birthDate')[i],
+            deathDate=players.get('deathDate')[i]
+        ))
+    f.close()
 
 
+def populate_awards_players(file_path):
+
+    columns = ["playerID", "award", "year", "lgID"]
+
+    awards_players = pd.read_csv('../data/awards_players.csv', usecols=columns)
+
+    f = open(file_path, "a")
+
+    for i in range(len(awards_players.get('playerID'))):
+        f.write("INSERT INTO awards_players(playerID, award, year, lgID) VALUES('{playerID}','{award}','{year}','{lgID}');\n".format(
+            playerID=awards_players.get('playerID')[i],
+            award=awards_players.get('award')[i],
+            year=awards_players.get('year')[i],
+            lgID=awards_players.get('lgID')[i]
+        ))
+
+    f.close()
 
 
-
-
-
-
-
-#convert_to_sql(test_path, awards_players)
+populate_teams_post(path)
+populate_coaches(path)
+populate_teams(path)
+populate_series_post(path)
+populate_players_teams(path)
+populate_players(path)
+populate_awards_players(path)
